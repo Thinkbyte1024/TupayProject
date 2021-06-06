201e9793583ad3322ae357633e7603d7b05d6ac2
Muhammad Aditya <23285884+Thinkbyte1024@users.noreply.github.com>
2021-06-06 11:50:07 +0800

    Penambahan case (Pilihan jurusan) pada TuitionBill

--- src/tupayproject/TuitionBill.java
+++ src/tupayproject/TuitionBill.java
@@ -6,8 +6,9 @@ public class TuitionBill {
 	// Method untuk SPP tetap
 	public static double TB_Fixed(int generationIndex, int facultyIndex) {
 		// Mendapatkan jumlah SPP variabel berdasarkan tipe "Fakultas"
+		// TODO: Buat cara untuk memeriksa pilihan angkatan secara efisien.
 		switch (facultyIndex) {
-			case 0:
+			case 0: // Faculty of Business and Humanities
 				switch (generationIndex) {
 					case 0:
 						return 2250000;
@@ -26,39 +27,39 @@ public class TuitionBill {
 						break;
 				}
 				break;
-			case 1:
+			case 1: // Faculty of Science and Technology
 				switch (generationIndex) {
 					case 0:
 						return 1750000;
 					case 1:
 						return 1800000;
 					case 2:
-						return 2050000;
+						return 1050000;
 					case 3:
-						return 2000000;
+						return 1005250;
 					case 4:
-						return 1950000;
+						return 1000000;
 					case 5:
-						return 1850000;
+						return 992000;
 					default:
 						System.out.println("Error");
 						break;
 				}
 				break;
-			case 2:
+			case 2: // Diploma Program
 				switch (generationIndex) {
 					case 0:
-						return 1750000;
+						return 1450000;
 					case 1:
-						return 1800000;
+						return 1200000;
 					case 2:
-						return 2050000;
+						return 1150000;
 					case 3:
-						return 2000000;
+						return 1000000;
 					case 4:
-						return 1950000;
+						return 950000;
 					case 5:
-						return 1850000;
+						return 850000;
 					default:
 						System.out.println("Error");
 						break;
@@ -72,19 +73,20 @@ public class TuitionBill {
 
 	// Method untuk SPP variabel
 	public static double TB_Variable(int generationIndex, int departmentIndex, double total_credits) {
+		// TODO: Buat cara untuk memeriksa pilihan angkatan secara efisien.
 		switch (departmentIndex) {
-			case 0:
+			case 0: // Informatics
 				switch (generationIndex) {
 					case 0:
-						return 190000 * total_credits;
+						return 320000 * total_credits;
 					case 1:
-						return 180000 * total_credits;
+						return 280000 * total_credits;
 					case 2:
-						return 170000 * total_credits;
+						return 260000 * total_credits;
 					case 3:
-						return 160000 * total_credits;
+						return 200000 * total_credits;
 					case 4:
-						return 150000 * total_credits;
+						return 180000 * total_credits;
 					case 5:
 						return 140000 * total_credits;
 					default:
@@ -92,10 +94,10 @@ public class TuitionBill {
 						break;
 				}
 				break;
-			case 1:
+			case 1: // Electrical Engineering
 				switch (generationIndex) {
 					case 0:
-						return 185000 * total_credits;
+						return 285000 * total_credits;
 					case 1:
 						return 175000 * total_credits;
 					case 2:
@@ -112,13 +114,88 @@ public class TuitionBill {
 				}
 				break;
 
-				// TODO: Tambahkan case untuk jurusan "Management", "Accounting", "Tourism" dan "Computer Engineering".
-
+			// TODO: Tambahkan case untuk jurusan "Management", "Accounting", "Tourism" dan "Computer Engineering".
+			case 2: // Management
+				switch (generationIndex) {
+					case 0:
+						return 285000 * total_credits;
+					case 1:
+						return 175000 * total_credits;
+					case 2:
+						return 165000 * total_credits;
+					case 3:
+						return 155000 * total_credits;
+					case 4:
+						return 145000 * total_credits;
+					case 5:
+						return 135000 * total_credits;
+					default:
+						System.out.println("Error");
+						break;
+				}
+				break;
+			case 3: // Accounting
+				switch (generationIndex) {
+					case 0:
+						return 285000 * total_credits;
+					case 1:
+						return 175000 * total_credits;
+					case 2:
+						return 165000 * total_credits;
+					case 3:
+						return 155000 * total_credits;
+					case 4:
+						return 145000 * total_credits;
+					case 5:
+						return 135000 * total_credits;
+					default:
+						System.out.println("Error");
+						break;
+				}
+				break;
+			case 4: // Tourism
+				switch (generationIndex) {
+					case 0:
+						return 285000 * total_credits;
+					case 1:
+						return 175000 * total_credits;
+					case 2:
+						return 165000 * total_credits;
+					case 3:
+						return 155000 * total_credits;
+					case 4:
+						return 145000 * total_credits;
+					case 5:
+						return 135000 * total_credits;
+					default:
+						System.out.println("Error");
+						break;
+				}
+				break;
+			case 5: // Computer Engineering
+				switch (generationIndex) {
+					case 0:
+						return 285000 * total_credits;
+					case 1:
+						return 175000 * total_credits;
+					case 2:
+						return 165000 * total_credits;
+					case 3:
+						return 155000 * total_credits;
+					case 4:
+						return 145000 * total_credits;
+					case 5:
+						return 135000 * total_credits;
+					default:
+						System.out.println("Error");
+						break;
+				}
+				break;
 			default:
+				// Tampilkan dialog error jika terdapat kesalahan dalam memilih kolom tersebut.
 				JOptionPane.showMessageDialog(null, "An unknown error has occured", "Error", JOptionPane.ERROR_MESSAGE);
 				break;
 		}
-
 		return 0;
 	}
 
